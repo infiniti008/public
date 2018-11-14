a5cf08bc8f7380aec409e86c854c2377a00dd9b0
vyacheslav_nikitenok <vyacheslav.nikitenok@vsgcommerce.com>
2018-11-14 19:04:43 +0300

    tyest

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', function(event) {
     console.log("install");
     self.addEventListener('fetch', function(event) {
-      console.log('fetching ->', event.request.url);
-      if(event.request.url.includes('/test')){
-          console.log("TRUE")
-            event.respondWith(
-                fetch('http://localhost:3000/index.html').then(function(response) {
-                    return response;
-                })
-            )
+      if(event.request.url.includes('/widget/tigiMyAccount/')){
+            console.log('fetching ->', event.request.url);
+  
+            // event.respondWith(
+            //     fetch('http://localhost:3000/index.html').then(function(response) {
+            //         return response;
+            //     })
+            // )
         }
     });
 });

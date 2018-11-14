c6c320e51a64dfc344c0592f20620db1f5b9a3df
vyacheslav_nikitenok <vyacheslav.nikitenok@vsgcommerce.com>
2018-11-14 18:35:52 +0300

    zxvxcvk

--- sw.js
+++ sw.js
@@ -10,21 +10,12 @@ self.addEventListener('install', function(event) {
     self.addEventListener('fetch', function(event) {
       console.log('fetching ->', event.request.url);
       if(event.request.url.includes('/test')){
-          console.log("TRUEEE")
+          console.log("TRUE")
             event.respondWith(
                 fetch('http://localhost:3000/index.html').then(function(response) {
                     return response;
                 })
             )
-                
-
-
-                // new Response('<h1>Hello from your friendly neighbourhood service worker!</h1>', {
-                //     headers: { 'Content-Type': 'text/html' }
-                //   })
-      } else {
-          console.log("FALSE")
-      }
-
+        }
     });
 });
